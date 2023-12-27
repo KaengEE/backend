5f8854e6720a1c83e60c73330b9c084b7948bf5e
KaengEE <yopuiqwer@naver.com>
2023-12-27 09:02:27 +0900

    주석 추가

--- src/main/java/com/mysite/backend/controller/UserController.java
+++ src/main/java/com/mysite/backend/controller/UserController.java
@@ -8,17 +8,21 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin("*") //cors 설정 - 모든링크
 @RequiredArgsConstructor
 public class UserController {
 
+    //생성자 주입
     private final UserRepository userRepository;
 
+    //새 사용자 리스트 추가
     @PostMapping("/users")
     User newUser(@RequestBody User newUser) {
+        //서버 요청 @RequestBody 서버 응답 @ResponseBody
         return userRepository.save(newUser);
     }
 
+    //모든 사용자 조회
     @GetMapping("/users")
     List<User> getAllUsers(){
         return userRepository.findAll();
