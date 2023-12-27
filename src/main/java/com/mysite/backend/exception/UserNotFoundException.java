fbb2e90b5a0486781efa1dfa4faaad13fc8b3b0e
KaengEE <yopuiqwer@naver.com>
2023-12-27 09:45:44 +0900

    id로 유저찾기 및 예외처리

---
+++ src/main/java/com/mysite/backend/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.mysite.backend.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(Long id) {
+        super("id에 맞는 유저가 없습니다." + id);
+    }
+}
